c1d5558646a35f6d8dbea2a58c29e751352a3191
grfatime <kurbanovhafatime@gmail.com>
2025-04-23 14:57:02 +0400

    completed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
 const buttons = document.querySelectorAll(".button0");
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     alert("Successful");
   });
+});
+
+btn.addEventListener("click", () => {
+  const email = input.value;
+
+  if (
+    !email.includes("@") ||
+    !email.includes(".") ||
+    email.indexOf("@") === 0 ||
+    email.endsWith("@") ||
+    email.endsWith(".")
+  ) {
+    alert("Zehmet olmasa duzgun e-mail daxil edin!");
+    input.focus();
+    input.value = "";
+  } else {
+    alert("E-mail qebul olundu: " + email);
+    input.value = "";
+    input.focus();
+  }
 });
